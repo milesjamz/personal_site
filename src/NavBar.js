6ba59fe3be616252fb93390c5be984e741ab2dc7
miles marillo <mjm03@hampshire.edu>
2019-12-03 16:48:07 -0500

    a little css

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 function NavBar() {
+
+
+const aboot = () => alert('ABOOT')
+const body = () => alert('BODY')
+const proojects = () => alert('PROOOOJECTS')
+const seevea = () => alert('SEE VEE HEE HEE')
+const etcetera = () => alert('ET CET ERRR UHH')
+
   return (
     <div className="navbar">
     * * m i l e s  - = - m a r i l l o  * *<br/>
-      <button> About Me </button> || <button> Body </button> || <button> Projects </button> ||<button>  CV </button> || <button> Etc </button>
+      <button onClick={() => aboot()}> About Me </button> || 
+      <button onClick={() => body()}> Body </button> || 
+      <button onClick={() => proojects()}> Projects </button> ||
+      <button onClick={() => seevea()}>  CV </button> || 
+      <button onClick={() => etcetera()}> Etc </button>
     </div>
   );
 }
