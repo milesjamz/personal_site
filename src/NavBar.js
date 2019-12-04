cd718debb1cc077338a052e2fe0a3b57dcfb0031
miles marillo <mjm03@hampshire.edu>
2019-12-04 13:45:36 -0500

    more

--- src/NavBar.js
+++ src/NavBar.js
@@ -12,11 +12,11 @@ const etcetera = () => alert('ET CET ERRR UHH')
   return (
     <div className="navbar">
     * * m i l e s  - = - m a r i l l o  * *<br/>
-      <button onClick={() => aboot()}> About Me </button> || 
-      <button onClick={() => body()}> Body </button> || 
-      <button onClick={() => proojects()}> Projects </button> ||
-      <button onClick={() => seevea()}>  CV </button> || 
-      <button onClick={() => etcetera()}> Etc </button>
+      <button onClick={() => aboot()}> <span style={{color:'yellow'}}>A</span> bout Me </button> || 
+      <button onClick={() => body()}> <span style={{color:'yellow'}}>B</span> ody </button> || 
+      <button onClick={() => proojects()}> <span style={{color:'yellow'}}>P</span> rojects </button> ||
+      <button onClick={() => seevea()}>  <span style={{color:'yellow'}}>C</span> V </button> || 
+      <button onClick={() => etcetera()}> <span style={{color:'yellow'}}>E</span> tc </button>
     </div>
   );
 }
