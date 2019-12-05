f9db1d035a26a4735f39352436e0b96c7ec3e38e
Miles Marillo <mjm03@hampshire.edu>
2019-12-05 15:16:22 -0500

    changed a bit

--- src/Projects.js
+++ src/Projects.js
@@ -5,15 +5,15 @@ function Projects() {
 	const colorsMa = ['blue','green','yellow',
 	'orange','red','pink','black','gold','grey','cyan','navy','aliceblue',
 	'antiquewhite','aquamarine','azure','beige','bisque','blueviolet','brown',
-	'chartreuse','chocolate','cornflowerblue','crimson','darkmagenta']
+	'chartreuse','chocolate','cornflowerblue','crimson','darkmagenta', 'fuschia']
   return (
     <div className="projects">
-      <p>You clicked {count} times</p>
+      <p>You're on color number {count}.</p>
       <button onClick={() => setCount(count + 1)}>
-        Click me to go up
+        Click me to go the next color
       </button>
       <button onClick={() => setCount(count - 1)}>
-        Click me to go down
+        Click me to go to the previous color
       </button><br/>
       <canvas width={640} height={425} 
       style={{"background-color":colorsMa[count] }} />
