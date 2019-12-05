ff105660fa971e504d1b51c9f4fe9de083fa87f3
Miles Marillo <mjm03@hampshire.edu>
2019-12-05 17:58:32 -0500

    added api project

--- src/Projects.js
+++ src/Projects.js
@@ -3,21 +3,39 @@ import React from 'react';
 class Projects extends React.Component {
 
   state = {
-    count: 0
+    count: 0,
+    clues: []
   }
+
+  myApi = 'http://jservice.io/api/clues'
 
 	colorsMa = ['blue','green','yellow',
 	'orange','red','pink','black','gold','grey','cyan','navy','aliceblue',
 	'antiquewhite','aquamarine','azure','beige','bisque','blueviolet','brown',
 	'chartreuse','chocolate','cornflowerblue','crimson','darkmagenta', 'fuschia']
   
-  tick = () => this.state.count === 25 ? this.setState({ count: 0 })  : this.setState ({ count: this.state.count + 1 })
+  tick = () => { 
+    this.state.count === 25 
+    ? this.setState({ count: 0 }) 
+    : this.setState ({ count: this.state.count + 1 })
+  console.log(this.state.count)
+  }
 
   componentDidMount() {
     setInterval( () => this.tick(), 1000 )
+    fetch(this.myApi)
+  .then(resp => resp.json())
+  .then(parsedResp => this.setState({ clues: parsedResp }) )
   }
 
   render() {
+    const showMyClues = () => {
+      console.log('emm')
+      return this.state.clues.map(clue => {
+        return <li> <b>{clue.question}</b> --- {clue.answer} </li>
+      })
+    }
+
   return (
     <div className="projects">
       <p>You're on color number {this.state.count}.</p>
@@ -28,43 +46,13 @@ class Projects extends React.Component {
         Click me to go to the previous color
       </button>< br/>
       <canvas width={640} height={425} 
-      style={{"background-color":this.colorsMa[this.state.count] }} />
+      style={{backgroundColor:this.colorsMa[this.state.count] }} />
+      <ul>
+      {showMyClues()}
+      </ul>
     </div>
   );
   }
 }
 
 export default Projects;
-
-// import React, { useState } from 'react';
-
-// function Projects() {
-// 	const [count, setCount] = useState(0);
-// 	const colorsMa = ['blue','green','yellow',
-// 	'orange','red','pink','black','gold','grey','cyan','navy','aliceblue',
-// 	'antiquewhite','aquamarine','azure','beige','bisque','blueviolet','brown',
-// 	'chartreuse','chocolate','cornflowerblue','crimson','darkmagenta', 'fuschia']
-  
-//   const tick = () => setCount(count + 1)
-
-//   const tickTock = () => {
-//     setInterval( () => tick(), 1000 )
-//   }
-
-//   return (
-//     <div className="projects">
-//       <p>You're on color number {count}.</p>
-//       <button onClick={() => setCount(count + 1)}>
-//         Click me to go the next color
-//       </button>
-//       <button onClick={() => setCount(count - 1)}>
-//         Click me to go to the previous color
-//       </button><br/>
-//       <canvas width={640} height={425} 
-//       style={{"background-color":colorsMa[count] }} />
-//       {tickTock()}
-//     </div>
-//   );
-// }
-
-// export default Projects;
