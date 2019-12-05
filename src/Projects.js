43bd84f4298b691c854ad376cc1678858b4f36cf
miles marillo <mjm03@hampshire.edu>
2019-12-05 13:06:22 -0500

    added hooks

--- src/Projects.js
+++ src/Projects.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Projects() {
+	const [count, setCount] = useState(0);
+	const colorsMa = ['blue','green','yellow',
+	'orange','red','pink','black','gold','grey','cyan','navy','aliceblue',
+	'antiquewhite','aquamarine','azure','beige','bisque','blueviolet','brown',
+	'chartreuse','chocolate','cornflowerblue','crimson','darkmagenta']
   return (
     <div className="projects">
-heees me pree jeekts thee
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me to go up
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        Click me to go down
+      </button><br/>
+      <canvas width={640} height={425} 
+      style={{"background-color":colorsMa[count] }} />
     </div>
   );
 }
