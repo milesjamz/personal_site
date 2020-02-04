7eac35b142ee165782f79ff93b2d812ad1582583
Miles Marillo <mjm03@hampshire.edu>
2020-02-04 17:27:58 -0500

    updatez

--- src/Etc.js
+++ src/Etc.js
@@ -3,23 +3,11 @@ import React from 'react';
 class Etc extends React.Component {
 
 state = {
-	formName: '',
 	formNameTwo: ''
-}
-
-handleChange = (e) => {
-	this.setState({ formName: e.target.value })
 }
 
 handleChangeTwo = e => {
 	this.setState({ formNameTwo: e.target.value })
-}
-
-handleSubmit = (e) => {
-	e.preventDefault()
-	let phrase = this.state.formName
-		let newPhrase = phrase.split('').join(' ').toLowerCase()
-	console.log(newPhrase)
 }
 
 handleSecondSubmit = e => {
@@ -29,24 +17,9 @@ handleSecondSubmit = e => {
 	console.log(this.state.formNameTwo)
 }
 
-newPhrase = () => {
-	return this.state.formName.split('').join(' ').toLowerCase()
-}
-
 render() {
   return (
     <div className="etc">
-    	<form onSubmit={this.handleSubmit} >
-    		<label>
-    		Enter text here to give it a e s t h e t i c<br/>
-    		<input type="text" 
-    		value={this.state.formName}
-    		onChange={this.handleChange}
-    		name="name" />
-    		</label>
-    		<input type="submit" value="Submit" />
-    	</form>
-		<br />
 		<form onSubmit={this.handleSecondSubmit} >
 			<label>
 				Enter text here to see if it's valid or not.<br />
@@ -57,7 +30,6 @@ render() {
 			</label>
 				<input type="submit" value="Submit" />
 		</form>
-    	{this.newPhrase()}
     </div>
   );
 	}
