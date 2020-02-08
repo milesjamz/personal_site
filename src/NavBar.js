3e6fe1178bf633c1a0bcb3df095b986f69921543
Miles Marillo <mjm03@hampshire.edu>
2020-02-08 18:29:21 -0500

    ch ch ch changes

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import {
-  Route,
   Link
 } from "react-router-dom";
-
-
 
 function NavBar() {
 
--- src/Practice.js
+++ src/Practice.js
@@ -16,3 +16,46 @@ class Practice extends React.Component {
 }
 
 export default Practice;
+
+
+state = {
+  count: 0,
+  clues: []
+}
+
+myApi = 'http://jservice.io/api/clues'
+
+colorsMa = ['blue','green','yellow',
+'orange','red','pink','black','gold','grey','cyan','navy','aliceblue',
+'antiquewhite','aquamarine','azure','beige','bisque','blueviolet','brown',
+'chartreuse','chocolate','cornflowerblue','crimson','darkmagenta', 'fuschia']
+
+componentDidMount() {
+  fetch(this.myApi)
+.then(resp => resp.json())
+.then(parsedResp => this.setState({ clues: parsedResp }) )
+}
+
+render() {
+  const showMyClues = () => {
+    console.log('emm')
+    return this.state.clues.map(clue => {
+      return <li> <b>{clue.question}</b> --- {clue.answer} </li>
+    })
+  }
+
+return (
+  <div className="projects">
+    <p>You're on color number {this.state.count}.</p>
+    <button onClick={() => this.setState({ count: this.state.count + 1 }) }>
+      Click me to go the next color
+    </button>
+    <button onClick={() => this.setState({ count: this.state.count - 1 }) }>
+      Click me to go to the previous color
+    </button>< br/>
+    <canvas width={640} height={425} 
+    style={{backgroundColor:this.colorsMa[this.state.count] }} />
+  </div>
+);
+}
+}
