f7f940f3c8858865a9c5e62d962abe0b95683fa2
Miles Marillo <mjm03@hampshire.edu>
2020-02-09 17:12:30 -0500

    project images added

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import GithubBtn from './GithubBtn'
+import Mythos from './mythos1.png'
+import Biblio from './bibliotecha1.png'
+import Hypo from './hypochond1.png'
+import BrewRev from './brewrev1.png'
 
 function Projects() {
 
@@ -10,10 +14,14 @@ function Projects() {
 Here are some of my projects:
 </p>
 <ul>
-  <li>mythOS - <GithubBtn link="https://github.com/milesjamz/mythosfrontend"/></li>
-  <li>brew review - <GithubBtn link="https://github.com/milesjamz/mod4frontend"/></li>
-  <li>bibliotecha - <GithubBtn link="https://github.com/milesjamz/bibliotecha"/></li>
-  <li>hypochondriapp - <GithubBtn link='https://github.com/milesjamz/hypochondriapp'/></li>
+  <li><img src={Mythos} alt='mythos' height="18%" width="18%"/>
+  <strong>mythOS</strong> - innovative web app to explore Greek Mythology <GithubBtn link="https://github.com/milesjamz/mythosfrontend"/></li>
+  <li><img src={BrewRev} alt='brew review' height="18%" width="18%"/>
+  <strong>brew review</strong> - <GithubBtn link="https://github.com/milesjamz/mod4frontend"/></li>
+  <li><img src={Biblio} alt='bibliotecha' height="18%" width="18%"/>
+  <strong>bibliotecha</strong> - <GithubBtn link="https://github.com/milesjamz/bibliotecha"/></li>
+  <li><img src={Hypo} alt='hypochondriapp' height="18%" width="18%"/>
+  <strong>hypochondriapp</strong> - <GithubBtn link='https://github.com/milesjamz/hypochondriapp'/></li>
 </ul>
     </div>
   );
