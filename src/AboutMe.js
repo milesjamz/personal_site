447a5c049585a883e5b74dc9a1f76ea8bfa2e65e
Miles Marillo <mjm03@hampshire.edu>
2020-02-24 10:45:33 -0500

    cleaned up files

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -5,8 +5,6 @@ function AboutMe() {
 
   const bioBlurb = "Full stack web developer with a love for telling stories through tech. Experienced in Rails, JavaScript and React/Redux, with a background in songwriting and recording. My passions include discovering compelling stories in data, and using technology to enhance, rather than reduce, our humanity."
 
-  const skills = ['JavaScript','Ruby','HTML','CSS','React','Rails','JWT Auth','git','RESTful APIs','Postgres', 'Node', 'Express', 'Redux']
-
   return (
     <div>
 <div className="aboutContainer">
@@ -16,13 +14,10 @@ function AboutMe() {
 {bioBlurb}</p>
 <strong>Skills</strong>
 <ul id="skills">
-{/* {skills.map((skill, index) => <li key={index}>{skill}</li>)} */}
 <Graph />
 </ul><br/>
 <div id="socialLinks">
 <strong>Links:</strong><br/>
-<a href="http://www.linkedin.com/in/milesjamz" target="_blank" rel="noopener noreferrer">linkedin</a>
-<br />
 <a href="http://www.github.com/milesjamz" target="_blank" rel="noopener noreferrer">github</a>
 </div>
 <br/><br/>
