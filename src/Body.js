143c6014cc2901480353849ed18ce0efdb68f421
Miles Marillo <mjm03@hampshire.edu>
2020-04-07 22:06:45 -0400

    yes

--- src/Body.js
+++ src/Body.js
@@ -5,7 +5,8 @@ function Body() {
     <div className="body">
       <strong>-=º¤ W E L C O M E ¤º=-</strong>
       <p>
-        This is the website for Miles Marillo, a full stack software engineer.
+        This is the website for Miles Marillo, a full stack software engineer.<br/>
+        I believe in a better world.
       </p>
     </div>
   );
