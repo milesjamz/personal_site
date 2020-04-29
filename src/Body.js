091a5dcd694c7b00b6a32b0b8611b0c75271ebef
Miles Marillo <mjm03@hampshire.edu>
2020-04-28 21:32:57 -0400

    update css and intro txt

--- src/Body.js
+++ src/Body.js
@@ -6,7 +6,7 @@ function Body() {
       <strong>-=º¤ W E L C O M E ¤º=-</strong>
       <p>
         This is the website for Miles Marillo, a full stack software engineer.<br/>
-        I believe in a better world.
+        I love telling stories, finding connections, and discovering secret narratives in mountains of information.
       </p>
     </div>
   );
