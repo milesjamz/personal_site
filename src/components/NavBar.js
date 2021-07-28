fd80b839c2f582900f0c310542dd7c2819e2ea3d
Miles Marillo <mjm03@hampshire.edu>
2021-07-28 09:41:01 -0400

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -23,7 +23,7 @@ function NavBar(props) {
 // }
   return (
     <div className="navbar">
-    <Link to='/' ><span id="nameTitle">- = M i l e s -=- M a r i l l o = -</span></Link> 
+    <Link to='/' ><span id="nameTitle">&nbsp; - = M i l e s   &nbsp;&nbsp;&nbsp;   M a r i l l o = - &nbsp; </span></Link> 
 <br/>       
       <div className='menuLinks'>
       <button><a href="https://www.github.com/milesjamz" target='_blank' rel='noopener noreferrer'>Github</a></button> <button><a href="https://www.linkedin.com/in/milesjamz" target='_blank' rel='noopener noreferrer'>LinkedIn</a></button> <button><a href='/static/media/mmarillo2021.999b7930.pdf' download>CV</a></button> <button><a href='mailto:milesjmarillo@gmail.com'>Contact Me</a></button>
