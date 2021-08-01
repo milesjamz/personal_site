25042411766c66a2e8d0d3a83a5253ee76a76599
Miles Marillo <mjm03@hampshire.edu>
2021-08-01 09:55:34 -0400

    body Update

--- src/Body.js
+++ src/Body.js
@@ -6,7 +6,7 @@ function Body(props) {
       <h1>Miles Marillo</h1>
       <h3>Fullstack Engineer</h3>
       <img src={props.pic} height="400em" alt='me, at the computer'></img>
-      <h3>About Me:</h3>
+      <h2>ABOUT:</h2>
       <p>
       Miles Marillo is a New York based full stack software engineer. <br/>
         He enjoys building products which help users lead richer, fuller, more human lives, not reduce them to extensions of an algorithm.<br/>
