9f888a06921ab33c0b839e712018a71c666d2393
Miles Marillo <mjm03@hampshire.edu>
2021-08-08 17:23:44 -0400

    responsive css

--- src/Body.js
+++ src/Body.js
@@ -3,23 +3,24 @@ import React from 'react';
 function Body(props) {
 
   let art = String.raw`
-  __  __ _ _             __  __            _ _ _     
+   __  __ _ _              __  __           _ _ _     
   |  \/  (_) |           |  \/  |          (_) | |    
   | \  / |_| | ___  ___  | \  / | __ _ _ __ _| | | ___ 
-  | |\/| | | |/ _ \/ __| | |\/| |/ _\` | '__| | | |/ _ \
-  | |  | | | |  __/\__ \ | |  | | (_| | |  | | | | (_) |
+  | |\/| | | |/ _ \/ __| | |\/| |/ _\`| '__| | | |/ _ \
+  | |  | | | |  __/\__ \ | |  | | (_| | |  | | | | (_)|
   |_|  |_|_|_|\___||___/ |_|  |_|\__,_|_|  |_|_|_|\___/ 
  `
   console.log(art);  
   return (
     <div className="body">
       <h1>
-{art}
+Miles Marillo
  </h1>
       <h3>Fullstack Engineer</h3>
       <img src={props.pic} height="400em" alt='me, at the computer'></img>
       <h2 >ABOUT:</h2>
       <p>
+        {art}
       Hey, I'm a New York based full stack software engineer. <br/>
         I enjoy building products that help users lead richer, fuller, more human lives, not reduce them to extensions of an algorithm.<br/>
         My interests are data visualization, intuitive and beautiful user experiences, and music.<br/>
